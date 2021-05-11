334ec23ec7b5263a91486fa4730c18840f51048c
ferrywu <ferrywu@arimacomm.com.tw>
2021-05-11 15:29:12 +0800

    test github pull request 2

--- helloworld.c
+++ helloworld.c
@@ -7,6 +7,6 @@ int main(int argc, char** argv)
     printf(", %s", argv[1]);
   }
   printf("\n");
-  printf("test github pull request 1\n");
+  printf("test github pull request 2\n");
   return 0;
 }
