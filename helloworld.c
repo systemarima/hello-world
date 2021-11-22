08922e89f9084725322d42b145b8e766c5940683
systemarima <84008993+systemarima@users.noreply.github.com>
2021-11-22 17:26:29 +0800

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -7,6 +7,6 @@ int main(int argc, char** argv)
     printf(", %s", argv[1]);
   }
   printf("\n");
-  printf("test github pull request 6 from ferrywu\n");
+  printf("test github from systemarima\n");
   return 0;
 }
